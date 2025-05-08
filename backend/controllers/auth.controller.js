dc7f78e9f3ac352404cf098db14f4facdb45bbf6
Daniel Valadez <danielvaladez2017@gmail.com>
2025-05-07 21:08:19 -0700

    Initial Commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import express from "express"; 
+
+
+export const signup = async (req, res, next) => {
+
+}
+
+export const login = async (req, res, next) => {
+  
+}
+
+export const logout = async (req, res, next) => {
+  
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"; 
+import dotenv from "dotenv"; 
+
+import authRoutes from "./routes/auth.routes.js"; 
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config(); 
+
+const app = express(); 
+
+const PORT = process.env.PORT || 5000; 
+
+app.use("/api/auth", authRoutes); 
+
+app.listen(PORT, (req, res) => {
+  console.group("app is listening on", PORT); 
+  connectMongoDB(); 
+})
