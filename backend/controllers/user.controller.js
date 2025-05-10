1ba041629c3d623571c89db8b12a4d605c86157a
Daniel Valadez <danielvaladez2017@gmail.com>
2025-05-10 14:10:16 -0700

    Working on user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,26 @@
+export const getUserProfile = async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.log("INTERNAL SERVER ERROR", error); 
+    res.status(500).json({error: error})
+  }
+}
+
+export const toggleFollowUser = async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.log("INTERNAL SERVER ERROR", error); 
+    res.status(500).json({error: error})
+  }
+}
+
+export const updateUserProfile = async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.log("INTERNAL SERVER ERROR", error); 
+    res.status(500).json({error: error})
+  }
+}
