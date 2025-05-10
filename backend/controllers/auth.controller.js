d93d2df2da92f5a985b8f85acdb94ab67636d393
Daniel Valadez <danielvaladez2017@gmail.com>
2025-05-09 19:52:13 -0700

    Added jwt for user signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,18 +1,61 @@
-import express from "express"; 
-
+//import express from "express";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 
 export const signup = async (req, res, next) => {
   try {
-    const {fullName, username, email, password} = req.body; 
+    const { fullName, username, email, password } = req.body;
+
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format." });
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken." });
+    }
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email has already been used." });
+    }
+
+    //Let's hash the user's password now
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+      username,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        following: newUser.following,
+        followers: newUser.followers,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data." });
+    }
   } catch (error) {
-
+    res.status(500).json({ error: "Internal server error." });
   }
-}
+};
 
-export const login = async (req, res, next) => {
-  
-}
+export const login = async (req, res, next) => {};
 
-export const logout = async (req, res, next) => {
-  
-}
+export const logout = async (req, res, next) => {};
